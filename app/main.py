997507866c165fdde8cda472decdecf71690d79b
piyush-mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2024-08-18 23:46:36 +0530

    chaining of inputs

--- app/main.py
+++ app/main.py
@@ -2,6 +2,11 @@ from transformers import pipeline
 
 # You can check any other model in the Hugging Face Hub. In my case I chose this one to classify text by positive and negative sentiment. 
 pipe = pipeline(model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
+def generate_response(prompt:str):
+   response = pipe("This is a great tutorial!")
+   label = response[0]["label"]
+   score = response[0]["score"]
+   return f"The '{prompt}' input is {label} with a score of {score}"
 
-print(pipe("I Love You And You are So great!"))
+print(generate_response("This tutorial is great!"))
 # Output[{'label': 'POSITIVE', 'score': 0.9998719692230225}]
