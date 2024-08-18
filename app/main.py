628dfd1f4ac4431810275a6fca1c9af16dda0d79
piyush-mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2024-08-19 00:08:52 +0530

    Api endpoint setup

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 from transformers import pipeline
 
-# You can check any other model in the Hugging Face Hub. In my case I chose this one to classify text by positive and negative sentiment. 
 pipe = pipeline(model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
-def generate_response(prompt:str):
-   response = pipe("This is a great tutorial!")
-   label = response[0]["label"]
-   score = response[0]["score"]
-   return f"The '{prompt}' input is {label} with a score of {score}"
 
-print(generate_response("This tutorial is great!"))
-# Output[{'label': 'POSITIVE', 'score': 0.9998719692230225}]
+app = FastAPI()
+
+class RequestModel(BaseModel):
+    input: str
+
+@app.post("/sentiment")
+def get_response(request: RequestModel):
+    prompt = request.input
+
+    response = pipe(prompt)
+
+    label = response[0]["label"]
+    score = response[0]["score"]
+    return f"The '{prompt}' input is {label} with a score of {score}"
